acd8ab39a3cbbd7b6d616d1b4fca346fe341252e
jcharles921 <babouwitonze921@gmail.com>
2025-05-29 16:09:10 +0200

    step 4

--- webapp/index.js
+++ webapp/index.js
@@ -1,14 +1,23 @@
+// sap.ui.define([
+// 	"sap/m/Button",
+// 	"sap/m/MessageToast"
+// ], (Button, MessageToast) => {
+// 	"use strict";
+
+// 	new Button({
+// 		text: "Ready...",
+// 		press() {
+// 			MessageToast.show("Hello World!");
+// 		}
+// 	}).placeAt("content");
+
+// });
 sap.ui.define([
-	"sap/m/Button",
-	"sap/m/MessageToast"
-], (Button, MessageToast) => {
+	"sap/ui/core/mvc/XMLView"
+], (XMLView) => {
 	"use strict";
 
-	new Button({
-		text: "Ready...",
-		press() {
-			MessageToast.show("Hello World!");
-		}
-	}).placeAt("content");
-
+	XMLView.create({
+		viewName: "ui5.quickstart.App"
+	}).then((oView) => oView.placeAt("content"));
 });
